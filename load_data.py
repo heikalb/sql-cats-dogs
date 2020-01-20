13c731c8491ca6a920f37b319de41ad0148fa9c5
heikalb <heikal93@gmail.com>
2020-01-19 23:10:15 -0800

    complete

---
+++ load_data.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import sqlite3
+
+
+def main():
+    # Create database
+    conn = sqlite3.connect('shelter_animals.db')
+    c = conn.cursor()
+
+    # Read data from original CSV
+    df = pd.read_csv('aac_shelter_outcomes.csv')
+
+    # Insert data into database
+    column_names = [c for c in df]
+    num_columns = len([c for c in df])
+
+    for index, row in df.iterrows():
+        values = [str(v).replace("\'", "") for v in row]
+        values = [f"\'{v}\'" for v in values]
+
+        sql_values = ", ".join(values)
+        c.execute(f"INSERT INTO animals VALUES ({sql_values})")
+
+    c.execute("SELECT * FROM animals")
+    print(c.fetchmany(10))
+
+    conn.commit()
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
